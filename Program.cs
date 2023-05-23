055d4787b40fd27c9b394d1d925cf4b334711f9d
Goomba <milan.raymakers@gmail.com>
2023-05-23 12:39:14 +0200

    v1.2

--- Program.cs
+++ Program.cs
@@ -28,7 +28,7 @@ namespace percentCool
         public static int savedLoopInt = 0;
         public static Dictionary<string, string> variables = new();
         public static Dictionary<string, List<string>> arrays = new();
-        public static string version = "1.1";
+        public static string version = "1.2";
         public static HttpListener listener;
         public static string url = "http://*:8000/";
         public static int pageViews = 0;
